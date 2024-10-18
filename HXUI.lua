56712534d1e60411a26f5ae6196cb8b056d275e2
Rag <mark.yz.yii@gmail.com>
2024-10-18 14:34:29 -0700

    Update version

--- HXUI.lua
+++ HXUI.lua
@@ -24,7 +24,7 @@
 
 addon.name      = 'HXUI';
 addon.author    = 'Team HXUI (Tirem, Shuu, colorglut, RheaCloud)';
-addon.version   = '1.1.1-onimitch4';
+addon.version   = '1.1.1-rag1';
 addon.desc      = 'Multiple UI elements with manager';
 addon.link      = 'https://github.com/tirem/HXUI'
 
